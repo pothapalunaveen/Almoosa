d4f6c488661b08629560e64fcde9c48b5f95da46
sandeep c <rupachenaka96@gmail.com>
2025-09-17 09:05:40 +0530

    Leadership/Service changes

--- almoosa/src/App.js
+++ almoosa/src/App.js
@@ -5,12 +5,13 @@ import { useTranslation } from 'react-i18next';
 import './i18n';
 import Footer from './components/Footer';
 import Header from './components/Header';
-import {BookAppointments }from "./components/BookAppointments";
+import { BookAppointments } from "./components/BookAppointments";
 import TopBar from './components/TopBar';
 import Login from './components/Login';
 import { useEffect } from 'react';
 import Almoosa from './components/Almoosa';
 import Service from './components/Service';
+import Leadership from './components/Leadership';
 
 function App() {
   const { t, i18n } = useTranslation();
@@ -21,36 +22,40 @@ function App() {
   };
 
   useEffect(() => {
-  document.documentElement.dir = i18n.language === 'ar' ? 'rtl' : 'ltr';
-}, [i18n.language]);
+    document.documentElement.dir = i18n.language === 'ar' ? 'rtl' : 'ltr';
+  }, [i18n.language]);
   return (
     <>
-     <div dir = {i18n.language === 'ar' ? "rtl" : "ltr"}> 
-     {/* <Footer/>  */}
-   
-    <Router>
-       <TopBar/>
-       <Header/> 
-       
-       
-       <Routes>
-          <Route path="/book-appointments" element={<BookAppointments />} />
+      <div dir={i18n.language === 'ar' ? "rtl" : "ltr"}>
+        {/* <Footer/>  */}
 
-        </Routes>
-         {/* <Login/>  */}
-         <Almoosa/>
-      
-       <Service/>  
-           <Footer/>   
-           
-    </Router> 
-     </div> 
-      </>
-  
+        <Router>
+          {/* <TopBar />
+          <Header /> */}
 
-    
-     
-    
+
+
+
+          <Routes>
+            <Route path="/book-appointments" element={<BookAppointments />} />
+            <Route path='/Leadership' element={<Leadership />} />
+
+          </Routes>
+          {/* <Login/>   */}
+          {/* <Almoosa />
+
+          <Service />
+          <Footer /> */}
+
+        
+        </Router>
+      </div>
+    </>
+
+
+
+
+
   );
 }
 
